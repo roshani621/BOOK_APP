1a529da8b7cd6372b37c66d64983db6241a572d3
roshani621 <146556071+roshani621@users.noreply.github.com>
2026-04-01 08:02:45 +0530

    Delete book_backend directory

--- book_backend/Model/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-client = MongoClient("mongodb://localhost:27017")
-db = client["mydatabase"]
-users_col = db["Users"]
-roles_col = db["Roles"]
-books_col = db["Books"]
-borrow_request_col = db["BorrowRequest"]
